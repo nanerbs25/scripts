70079295780f73aabb24c31c78654fb178db3b73
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-12 18:14:03 +0800

    t

--- fixes1.sh
+++ fixes1.sh
@@ -6,8 +6,8 @@
 
 
 cd frameworks/base/
-git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-3
-git cherry-pick 61723d6220ad417008c50565bc9212e9cec96450
+git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-10
+git cherry-pick 53f9d4676c9d38c53d88942c201ec9283a439533
 cd ../../
 
 
--- sync1.sh
+++ sync1.sh
@@ -13,10 +13,6 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
 source scripts/fixes.sh
-lunch lineage_h872-userdebug
-m bacon
-
-find out/target/product/*/ -type f -name "*.zip" -execdir bash -c 'mv "$1" "${1%.zip}_newname.zip"' bash {} \;
 source scripts/fixes1.sh
 lunch lineage_h872-userdebug
 m bacon
