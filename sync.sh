293b9b4cbd013a76a9b3878cc4030a425a02f0a6
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-12 12:46:59 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-rm -rf build/make/
+
 rm -rf frameworks/base/
 rm -rf device/lge/
 rm -rf hardware/lge
-rm -rf kernel/lge/msm8996/
 rm -rf vendor/lge/
 rm -rf vendor/lineage/
 rm out/target/product/*/*.zip
