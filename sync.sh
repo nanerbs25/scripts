308241e9151155d833539e0567157b98e026eaae
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-01-17 05:27:34 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-rm -rf build/make/
-rm -rf frameworks/base/
-rm -rf vendor/lineage/
 
-rm out/target/product/*/*.zip
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 source build/envsetup.sh
 
